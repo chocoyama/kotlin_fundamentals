4066d85656221cb542439e407c710c70cc146a6b
Takuya Yokoyama <takyokoy@yahoo-corp.jp>
2019-08-23 16:23:28 +0900

    Android Kotlin Fundamentals 01.3まで

--- app/src/main/java/com/example/android/kotlin_fundamental/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlin_fundamental/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android.kotlin_fundamental
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
@@ -9,19 +10,31 @@ import java.util.*
 // MEMO: AppCompatActivityは後方互換性も考慮した上で全ての最新機能を利用できるようにするために用いる
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // MEMO: LayoutInflationはXMLファイルをViewObjectに変換するための処理
         setContentView(R.layout.activity_main)
+
+        // findViewByIdは重たい操作
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
