3123386584425df3106578957a7a8ff55b8e5fcb
Takuya Yokoyama <puripuripuririnrinririn@gmail.com>
2019-09-03 18:29:19 +0900

    Android Kotlin Fundamentals 03.2: Define navigation paths

--- app/src/main/java/activity/TriviaActivity.kt
+++ app/src/main/java/activity/TriviaActivity.kt
@@ -19,14 +19,30 @@ package activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.kotlin_fundamental.R
 import com.example.android.kotlin_fundamental.databinding.ActivityTriviaBinding
 
 class TriviaActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityTriviaBinding>(this, R.layout.activity_trivia)
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
     // TODO (01) Create the new TitleFragment
