0c2119fb62985b93c79f7df6e0a4dbfa8e4bb6f8
Takuya Yokoyama <puripuripuririnrinririn@gmail.com>
2019-09-15 16:18:49 +0900

    Android Kotlin Fundamentals 09.2: WorkManager

---
+++ app/src/main/java/work/RefreshDataWorker.kt
@@ -0,0 +1,30 @@
+package work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import database.getDatabase
+import repository.VideosRepository
+import retrofit2.HttpException
+import timber.log.Timber
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "com.example.android.kotlin_fundamental.work.RefreshDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+
+        try {
+            repository.refreshVideos()
+            Timber.d("Work request for sync is run")
+        } catch (e: HttpException) {
+            return Result.retry()
+        }
+        return Result.success()
+    }
+
+}
